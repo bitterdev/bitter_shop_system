98a88e6d630d0d5413c5a42ffa6ac00ecb5b21cf
Bitter Digital Solutions UG (haftungsbeschränkt) <fabian@bitter.de>
2021-09-22 15:51:02 +0200

    Update SavedProductSearch.php

--- src/Bitter/BitterShopSystem/Entity/Search/SavedProductSearch.php
+++ src/Bitter/BitterShopSystem/Entity/Search/SavedProductSearch.php
@@ -19,7 +19,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="`SavedProductSearchQueries`")
+ * @ORM\Table(name="`ShopSystemSavedProductSearchQueries`")
  */
 class SavedProductSearch extends SavedSearch
 {
