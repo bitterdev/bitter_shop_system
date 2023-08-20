c9a4bfc2fc44aab67f44584a001b697da9975688
Fabian Bitter <fabianbitter@MacBook-Air-von-Fabian-2.local>
2023-08-20 12:59:40 +0200

    Update V9

--- controller.php
+++ controller.php
@@ -26,8 +26,8 @@ use Exception;
 class Controller extends Package implements ProviderAggregateInterface
 {
     protected $pkgHandle = 'bitter_shop_system';
-    protected $appVersionRequired = '8.5.4';
-    protected $pkgVersion = '2.0.0';
+    protected $appVersionRequired = '9.2.0';
+    protected $pkgVersion = '3.0.0';
     protected $pkgAutoloaderRegistries = [
         'src/Bitter/BitterShopSystem' => 'Bitter\BitterShopSystem',
         'src/Bitter/BitterShopSystem/Attribute/Key' => 'Concrete\Package\BitterShopSystem\Attribute\Key'
